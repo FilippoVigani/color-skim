426718ec0943f88d5b5c5e9377ead51d9f719ddf
Filippo Vigani <vigani.filippo@gmail.com>
2022-04-03 19:09:01 +0200

    Rework kmeans++ implementation

--- src/main/kotlin/kmeans/initialization/KMeansPlusPlus.kt
+++ src/main/kotlin/kmeans/initialization/KMeansPlusPlus.kt
@@ -11,26 +11,24 @@ fun kmeansPlusPlus(k: Int, points: Array<Point>): Array<Int> {
     var centersCount = 1
     while (centersCount < k){
         var cumulativeDistance = 0.0
-        val distances = Array(points.size) { p ->
-            var minDistance: IndexedValue<PointDistance>? = null
+        val cumulativeDistances = Array(points.size) { p ->
+            var minDistance = PointDistance.POSITIVE_INFINITY
             for(i in centersIndexes.indices){
                 if (centersIndexes[i] != -1){
                     val distance = euclideanDistanceSquared(points[centersIndexes[i]], points[p])
-                    if (distance <= (minDistance?.value ?: PointDistance.MAX_VALUE)){
-                        minDistance = IndexedValue(i, distance)
+                    if (distance <= minDistance){
+                        minDistance = distance
                     }
                 }
             }
-            cumulativeDistance += minDistance!!.value
-            minDistance.value
+            cumulativeDistance += minDistance
+            cumulativeDistance
         }
-        var randomDistanceTotal = random.nextDouble(cumulativeDistance)
+        val randomCumulativeDistance = random.nextDouble(cumulativeDistance)
         var p = -1
-        while (randomDistanceTotal >= 0){
+        do {
             p++
-            val currentDistance = distances[p]
-            randomDistanceTotal -= currentDistance
-        }
+        } while (randomCumulativeDistance > cumulativeDistances[p])
         centersIndexes[centersCount++] = p
     }
     return centersIndexes
